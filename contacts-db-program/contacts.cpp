ab76956adca4e9950775c51c7af388adf3cb0baf
Arjun Pramanik <arjun29.pramanik@gmail.com>
2025-01-08 21:59:16 -0500

    complete contacts-db-program, modify .gitignore to ignore respective output file

--- contacts-db-program/contacts.cpp
+++ contacts-db-program/contacts.cpp
@@ -14,6 +14,19 @@ int main()
     // declaring output file stream (to write data to a file)
     ofstream output;
 
+    // clear existing contacts.txt file if selected
+    char decision;
+    cout << "Would you like to clear the existing contact sheet, if it exists? (y/n): ";
+    cin >> decision;
+    if (decision == 'y' || decision == 'Y')
+    {
+        output.open("contacts.txt");
+        output << "";
+        output.close();
+
+        cout << "\nContact sheet cleared!\n";
+    }
+
     // open file for output operations
     // output APPENDED to the end of file called 'contacts.txt'
     output.open("contacts.txt", ios::app);
@@ -22,10 +35,12 @@ int main()
     string name = "";
     string phoneNumber = "";
 
-    cout << "Enter the name of the contact: \n";
-    cin >> name;
-    cout << "Enter the number of the contact: \n";
-    cin >> phoneNumber;
+    // prompt for name and phone number
+    cin.ignore();    // discard the "leftover" newline from the cin for decision
+    cout << "\nEnter the name of the contact: ";
+    getline(cin, name); 
+    cout << "\nEnter the number of the contact: ";
+    getline(cin, phoneNumber); 
 
     // putting user input into output file
     output << "Name: " << name << "\n" << "Phone Number: " << phoneNumber << "\n\n";
@@ -34,7 +49,11 @@ int main()
     output.close();
 
     // ending message with information on where to find output
-    cout << "\nDone. You will find this entry and all previous entries in " << "the file 'contacts.txt' in the current directory.\n";
-
+    if (decision == 'y')
+    {
+        cout <<"\nDone. You will find this entry in the file 'contacts.txt' in the current directory.\n";
+    } else { 
+        cout <<"\nDone. You will find this and all previous entries in the file 'contacts.txt' in the current directory.\n";
+    }
     return 0;
 }
